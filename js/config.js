77cd73a0ce1c47f0bd53a630539ec6cb272f4722
Haipa Wahmi <haipawahmi60@gmail.com>
2026-09-13 23:43:32 +0700

    Update application configuration details

--- js/config.js
+++ js/config.js
@@ -1,11 +1,11 @@
 // =====================================================
-// CEPA - Konfigurasi Aplikasi
+// Cepa App - Konfigurasi Aplikasi
 // Ganti APPS_SCRIPT_URL dengan URL Web App Google Apps Script Anda
 // =====================================================
-const ID_PENERAPAN = 'AKfycbyKXq30agO5682qQkic_gKXO3JxkrQtN1BQk78WsKZXHTYDuwbCDOgr9L8VMzOMRVBLxw';
+const ID_PENERAPAN = 'AKfycby5pBaDiowA6GTlc1wyHGSJTZ79UecXtup0HeE4S920Frkbb8kFew8GQnWFsIRcp0STUQ';
 const CONFIG = {
   APPS_SCRIPT_URL: 'https://script.google.com/macros/s/' + ID_PENERAPAN + '/exec',
-  APP_NAME: 'CEPA',
+  APP_NAME: 'Cepa App',
   APP_FULL_NAME: 'Sistem Informasi Manajemen Peminjaman Alat',
   COMPANY: 'PT. Wahmi Teknologi Indonesia',
   VERSION: '1.0.0'
